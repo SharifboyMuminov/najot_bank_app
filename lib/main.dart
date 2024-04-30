b57918302adccef8fc59e1c35a1e2ac499ece801
Sharifjon <sharifboymuminov@gmail.com>
2024-04-30 22:50:30 +0500

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
+import 'dart:async';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
+import 'package:untitled1/server/firebase_options.dart';
 import 'screens/home_screen/home_screen.dart';
 
-void main(List<String> args) {
-  runApp(MyApp());
+Future<void> main(List<String> args) async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(const MyApp());
 }
-
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -24,7 +29,7 @@ class MyApp extends StatelessWidget {
           home: child,
         );
       },
-      child: HomeScreen(),
+      child: const HomeScreen(),
     );
   }
 }
